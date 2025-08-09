53170f6d99d85d4d10c64af8a019d4cac6382948
gorbiakv-creator <gorbiakv@gmail.com>
2025-08-09 16:51:39 +0300

    Add files via upload

---
+++ surs.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  var hostrc = [
+    "https://rc.bwa.to/online.js?v26062025",
+    "https://rc.bwa.to/sisi.js?v26062025",
+    "https://adultjs.onrender.com",
+    "https://sisi.am/pussy.js?v26062025",
+    "https://uafix.net"
+  ];
+
+  Lampa.Utils.putScriptAsync([
+    "https://rc.bwa.to/online/js/ta9jmtj",
+    "https://rc.bwa.to/sisi/js/ta9jmtj",
+    "https://adultjs.onrender.com",
+    "https://sisi.am/pussy.js?v26062025",
+    "https://uafix.net"
+  ], function() {});
+})();
